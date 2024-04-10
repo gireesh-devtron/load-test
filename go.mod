bd0b86ef9ce35a8a8911ed5abbea5bbef068f7c9
Gireesh Naidu <gireesh@devtron.ai>
2024-04-10 12:22:41 +0530

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gireesh-devtron/load-test
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,29 @@
+func main() {
+	ch := make(chan struct{})
+	fn := func(n int) {
+		for i := 0; i < n; i++ {
+			s := ""
+			m := &sync.Mutex{}
+			for j := 0; j < 100; j++ {
+				go func() {
+					defer m.Unlock()
+					m.Lock()
+					s = s + fmt.Sprintf("hello world-%d-%d ", i, j)
+					fmt.Println(s)
+				}()
+			}
+		}
+	}
+
+	http.HandleFunc("/load", func(w http.ResponseWriter, r *http.Request) {
+		ns := r.URL.Query().Get("n")
+		n, _ := strconv.Atoi(ns)
+		fn(n)
+		fmt.Fprint(w, "success")
+	})
+
+	go func() {
+		err := http.ListenAndServe(":8080", nil)
+		log.Fatal(err)
+	}()
+	<-ch
